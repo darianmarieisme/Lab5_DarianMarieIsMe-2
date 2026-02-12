4185d738e28f6c2dd9535190e84aa8ed7b1046d7
Darian Bruce <darianmariebruce@gmail.com>
2026-02-12 17:04:16 -0500

    working on game logic

--- Lab5_DarianMarieIsMe-2.py
+++ Lab5_DarianMarieIsMe-2.py
@@ -14,8 +14,26 @@ min = 1
 
 max = 4
 
+player1_turn = True
+
+value_error = True
+
 #game instructions
 
 print("Welcome to Pick Up Sticks.\nThis game is for 2 players. \nEach player will",
       f"take turns picking up from\n{min} to {max} sticks from a pile of 13 sticks.",
       "\nWhoever picks up the last stick wins!")
+
+while sticks > 0:
+    if player1_turn:
+        print("It is player 1's turn!"
+              ,"\nEnter the amount of sticks you want to pick up.")
+        while value_error:
+             player1 = int(input(f"The value can only be between {min} and {max}."))
+             if not (1 <= player1 <= 4):
+                print("This is an invalid value. Please try again.")
+             else:
+                 sticks = sticks - player1
+                 print(f"There are {sticks} left.")
+                 value_error = False
+            
