d1426f91569d27c411f9a48ea8b878c8e1de7de7
Darian Bruce <darianmariebruce@gmail.com>
2026-02-12 16:42:04 -0500

    instructions written

--- Lab5_DarianMarieIsMe-2.py
+++ Lab5_DarianMarieIsMe-2.py
@@ -5,3 +5,17 @@ This is a game to pick up a various amount of sticks and whover picks up the las
 wins
 02/12/2026
 '''
+
+#game variables
+
+sticks = 13
+
+min = 1
+
+max = 4
+
+#game instructions
+
+print("Welcome to Pick Up Sticks.\nThis game is for 2 players. \nEach player will",
+      f"take turns picking up from\n{min} to {max} sticks from a pile of 13 sticks.",
+      "\nWhoever picks up the last stick wins!")
