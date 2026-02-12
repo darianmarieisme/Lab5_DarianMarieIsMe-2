6d4e556dfb26d98d091ebd4fecee711a4eebd58c
Darian Bruce <darianmariebruce@gmail.com>
2026-02-12 17:29:28 -0500

    finished player 2 logic

--- Lab5_DarianMarieIsMe-2.py
+++ Lab5_DarianMarieIsMe-2.py
@@ -16,13 +16,13 @@ sticksmax = 4
 
 player1_turn = True
 
-value_error = True
-
 invalid_print = "This is an invalid value. Please try again."
 
 input_instructions = "\nEnter the amount of sticks you want to pick up."
 
 valid_input_instructions = f"The value can only be between {sticksmin} and {sticksmax}."
+
+sticks_left = f"There are {sticks} sticks left."
 
 #game instructions
 
@@ -32,14 +32,15 @@ print("Welcome to Pick Up Sticks.\nThis game is for 2 players. \nEach player wil
 
 while sticks > 0:
     if player1_turn:
+        value_error = True
         print("It is player 1's turn!", input_instructions)
         while value_error:
              sticks_in_turn_amount = int(input(valid_input_instructions))
-             if not (1 <= sticks_in_turn_amount <= 4):
+             if not (1 <= sticks_in_turn_amount <= 4 and sticks_in_turn_amount >= sticks):
                 print(invalid_print)
              else:
                  sticks = sticks - sticks_in_turn_amount
-                 print(f"There are {sticks} sticks left.")
+                 print(sticks_left)
                  player1_turn = False
                  value_error = False
     else:
@@ -47,6 +48,11 @@ while sticks > 0:
         value_error = True
         while value_error: 
             sticks_in_turn_amount = int(input(valid_input_instructions))
-            if not (1 <= sticks_in_turn_amount <= 4):
+            if not (1 <= sticks_in_turn_amount <= 4 and sticks_in_turn_amount >= sticks):
                 print(invalid_print)
+            else:
+                sticks = sticks - sticks_in_turn_amount
+                print(sticks_left)
+                value_error = False
+                player1_turn = True
 
